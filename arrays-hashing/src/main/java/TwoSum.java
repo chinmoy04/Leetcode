75cbf7488f4f9734c5e08390225e413738550493
chinmoy04 <chinmoy.220411@gmail.com>
2024-02-28 12:55:09 +0530

    Solution for two Sum

---
+++ arrays-hashing/src/main/java/TwoSum.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        //initialization
+        int[] ans = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(target-nums[i])){
+                ans[0] = i;
+                ans[1] = map.get(target-nums[i]);
+                return ans;
+            }else{
+                map.put(nums[i], i);
+            }
+        }
+
+        return ans;
+    }
+}
